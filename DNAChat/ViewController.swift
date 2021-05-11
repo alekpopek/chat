4386eb4c877fee4648995bfc687f4cd76b634c7b
Aleksander Popek <aleksander.popek@altermind.pl>
2021-05-11 20:08:04 +0200

    Initial Commit

---
+++ DNAChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DNAChat
+//
+//  Created by Aleksander Popek on 11/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
